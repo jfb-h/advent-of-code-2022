79c6fcfbe76ccdbc3c60e99a9f0eadf68f1ba953
Jakob Hoffmann <jfb-hoffmann@hotmail.de>
2022-12-05 12:50:59 +0100

    simplify

--- day 4/script.jl
+++ day 4/script.jl
@@ -1,17 +1,16 @@
-using CSV, Tables
-
-readdata(file) = CSV.File(file; header=["first", "second"], delim=',') |> Tables.rowtable
+splitline(line) = split(line, ",")
 parserange(s) = range(parse.(Int, split(s, "-"))...)
 completeoverlap(x, y) = issubset(x, y) || issubset(y, x)
 partialoverlap(x, y) = !isempty(intersect(x, y))
 
-calculate(overlapfun, file) = mapreduce(+, readdata(file)) do row
-    overlapfun(parserange(row.first), parserange(row.second))
+calculate(overlapfun, file) = mapreduce(+, eachline(file)) do line
+    firstelf, secondelf  = parserange.(splitline(line))
+    overlapfun(firstelf, secondelf)
 end
 
 part1(file) = calculate(completeoverlap, file)
 part2(file) = calculate(partialoverlap, file)
 
-part1("data.txt")
-part2("data.txt")
+part1("day 4/data.txt")
+part2("day 4/data.txt")
 
