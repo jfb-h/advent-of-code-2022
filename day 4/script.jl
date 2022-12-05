da04bf11ac7e25a3b4098e4e043f4e86ab3d2f59
Jakob Hoffmann <jfb-hoffmann@hotmail.de>
2022-12-05 19:37:19 +0100

    sum instead of mapreduce

--- day 4/script.jl
+++ day 4/script.jl
@@ -3,14 +3,15 @@ parserange(s) = range(parse.(Int, split(s, "-"))...)
 completeoverlap(x, y) = issubset(x, y) || issubset(y, x)
 partialoverlap(x, y) = !isempty(intersect(x, y))
 
-calculate(overlapfun, file) = mapreduce(+, eachline(file)) do line
-    firstelf, secondelf  = parserange.(splitline(line))
-    overlapfun(firstelf, secondelf)
+function calculate(overlapfun, file)
+    sum(eachline(file)) do line
+        firstelf, secondelf  = parserange.(splitline(line))
+        overlapfun(firstelf, secondelf)
+    end
 end
 
 part1(file) = calculate(completeoverlap, file)
 part2(file) = calculate(partialoverlap, file)
 
-part1("day 4/data.txt")
-part2("day 4/data.txt")
-
+part1("data.txt")
+part2("data.txt")
